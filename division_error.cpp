dae4bccc6176671b618ac7b24fb8867e37da7d10
ahad-hossin <78794797+ahad-hossin@users.noreply.github.com>
2021-02-13 20:41:23 +0600

    Create division_error.cpp

---
+++ division_error.cpp
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+
+int main(){
+
+float a ,b , result ;
+
+printf("enter 1st number");
+scanf("%f", &a);
+
+printf("enter 2nd number");
+scanf("%f", &b);
+
+if (b==0)
+{
+    printf("ERROR");
+}
+else{
+    result = a/b;
+
+    printf("result %f ", result );
+}
+return 0;
+}
