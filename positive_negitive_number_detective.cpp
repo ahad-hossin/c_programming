6f2bd3011317e3f21394077bbbf79ec45b24d0b0
ahad-hossin <78794797+ahad-hossin@users.noreply.github.com>
2021-02-13 21:09:00 +0600

    Create positive_negitive_number_detective.cpp

---
+++ positive_negitive_number_detective.cpp
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+
+int main(){
+
+int a ,b  ;
+
+int result ;
+
+printf("enter number: ");
+scanf("%d", &a);
+
+
+if (a>0){
+    printf("positive");
+}
+
+else {
+    printf("negative");
+}
+
+
+return 0;
+}
