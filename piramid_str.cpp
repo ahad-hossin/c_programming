f8ea948ef67ac62ea66310d8664f0b0c8c928c09
ahad-hossin <78794797+ahad-hossin@users.noreply.github.com>
2021-03-08 17:24:37 +0600

    Create piramid_str.cpp

---
+++ piramid_str.cpp
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+int main() {
+
+    int n;
+
+    scanf("%d",&n);
+
+    int temp = n;
+
+    for(int i = 0;i<n;i++){
+
+        for(int j =i;j<n-1;j++){
+
+            printf(" ");
+
+        }
+
+        for(int k=0;k<2*i+1;k++){
+            printf("*");
+        }
+
+        printf("\n");
+
+    }
+
+    return 0;
+}
